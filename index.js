5218c1c3401089fe2d27477fba3f57298db52092
EUGENE AIJA <eugeneosu7@gmail.com>
2022-11-25 11:36:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Aija';
+
+const welcomeMessage = 'Welcome to Flatbook, + Aija!' ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, ${A}!" ;
